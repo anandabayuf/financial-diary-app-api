38c988fc87363148286fd8f9e4bdb2138753bb49
Ananda Bayu <anandabayu090@gmail.com>
2023-06-12 09:26:23 +0700

    fix env

--- constants/constants.js
+++ constants/constants.js
@@ -5,8 +5,8 @@ config();
 const SECRET_KEY = process.env.SECRET_KEY;
 const MONGO_URI = process.env.MONGO_URI;
 
-const PUBLIC_KEY = process.env.PUBLIC_KEY;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const PUBLIC_KEY = process.env.PUBLIC_KEY.split(String.raw`\n`).join("\n");
+const PRIVATE_KEY = process.env.PRIVATE_KEY.split(String.raw`\n`).join("\n");
 
 const NODEMAILER_AUTH_USER = process.env.NODEMAILER_AUTH_USER;
 const NODEMAILER_AUTH_PASS = process.env.NODEMAILER_AUTH_PASS;
